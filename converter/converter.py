49858c282c9a554e449dead915d2214e83093245
Robin <vjc.robin@gmail.com>
2020-08-23 13:52:48 +1000

    implemented functionality to return currency amounts of both dollars and cents now converter also handles negative numbers improved docstrings added __init__.py to allow importing code as a package created constants.py to capture all required constant strings added version check; code uses python 3 functionality

--- converter/converter.py
+++ converter/converter.py
@@ -1,6 +1,8 @@
-from converter.constants import MAGNITUDES
-from converter.constants import TENS
-from converter.constants import UNITS
+import sys
+from converter.constants import MAGNITUDES, TENS, UNITS
+
+if sys.version_info[0] < 3:
+    raise Exception('This application requires Python 3')
 
 
 def _less_than_hundred_to_words(number):
@@ -28,10 +30,8 @@ def _less_than_thousand_to_words(number):
             return f'{UNITS[number_hundreds]} hundred'
 
 
-def int_to_words(number):
-    """
-    Converts integer into words;
-    """
+def _positive_int_to_words(number):
+    """ Converts positive integer numbers into words, e.g. number = 99 returns 'ninety nine'. """
     if number < 1000:
         return _less_than_thousand_to_words(number).strip()
 
@@ -47,13 +47,31 @@ def int_to_words(number):
     # if we have number = 1_000_000_000, magnitude is in [3, 2, 1]
     for magnitude in reversed(range(1, number_magnitude+1)):
         number_xillions = number // 1000**magnitude
-        number_xillions_in_words = int_to_words(number_xillions)
+        number_xillions_in_words = _positive_int_to_words(number_xillions)
 
         remainder = number % 1000**magnitude
-        remainder_in_words = int_to_words(remainder)
+        remainder_in_words = _positive_int_to_words(remainder)
 
         if remainder_in_words:
             return f'{number_xillions_in_words} {MAGNITUDES[magnitude]}, {remainder_in_words}'.strip()
         else:
             return f'{number_xillions_in_words} {MAGNITUDES[magnitude]}'.strip()
 
+
+def _int_to_words(number):
+    if number == 0:
+        return 'zero'
+    if number < 0:
+        return f'minus {_positive_int_to_words(-number)}'
+    else:
+        return _positive_int_to_words(number)
+
+
+def currency_amount_to_words(currency_amount):
+    number_of_dollars = int(currency_amount)
+    number_of_cents = int((currency_amount * 100 - int(number_of_dollars) * 100))
+
+    number_of_dollars_in_words = _int_to_words(number_of_dollars)
+    number_of_cents_in_words = _int_to_words(number_of_cents)
+
+    return f'{number_of_dollars_in_words} DOLLARS AND {number_of_cents_in_words} CENTS'
--- tests/test_converter.py
+++ tests/test_converter.py
@@ -1,15 +1,19 @@
-from converter.converter import int_to_words
+from converter.converter import _int_to_words, currency_amount_to_words
 
 
-def test_converter():
+def test_int_to_words():
+    """ Test of function _int_to_words() """
+
     test_int_numbers = {
-        # 0: 'zero',
+        # assert that we correctly map -0 -> 0 and return 'zero' instead of 'minus zero'
+        -0: 'zero',
         10: 'ten',
         14: 'fourteen',
         34: 'thirty four',
         99: 'ninety nine',
         100: 'one hundred',
         101: 'one hundred and one',
+        -133: 'minus one hundred and thirty three',
         453: 'four hundred and fifty three',
         1000: 'one thousand',
         1999: 'one thousand, nine hundred and ninety nine',
@@ -18,10 +22,29 @@ def test_converter():
         1_989_999: 'one million, nine hundred and eighty nine thousand, nine hundred and ninety nine',
         1_357_256: 'one million, three hundred and fifty seven thousand, two hundred and fifty six',
         3_000_000_000: 'three billion',
-        7_221_300_000: 'seven billion, two hundred and twenty one million, three hundred thousand'
+        7_221_300_000: 'seven billion, two hundred and twenty one million, three hundred thousand',
+        -7_221_300_000: 'minus seven billion, two hundred and twenty one million, three hundred thousand',
     }
 
+    # verify that we map the integer amounts to the expected strings
     for int_number in test_int_numbers:
-        int_number_in_words = int_to_words(int_number)
+        int_number_in_words = _int_to_words(int_number)
         print(int_number, ': ', int_number_in_words)
         assert int_number_in_words == test_int_numbers[int_number]
+
+
+def test_currency_amount_to_words():
+    """ Test of converter function currency_amount_to_words() """
+
+    test_currency_amounts = {
+        345.78: 'three hundred and forty five DOLLARS AND seventy eight CENTS',
+        -345.00: 'minus three hundred and forty five DOLLARS AND zero CENTS',
+        0: 'zero DOLLARS AND zero CENTS',
+        1_100_452.30: 'one million, one hundred thousand, four hundred and fifty two DOLLARS AND thirty CENTS'
+    }
+
+    # verify that we map the currency amounts to the expected strings
+    for currency_amount in test_currency_amounts:
+        amount_in_words = currency_amount_to_words(currency_amount)
+        print(currency_amount, ': ', amount_in_words)
+        assert amount_in_words == test_currency_amounts[currency_amount]
